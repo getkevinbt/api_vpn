f217486b15a0142ba000971e8189a64d7deb1cc5
Mc. Denci Dance <mc.dencidance@gmail.com>
2023-07-27 21:48:29 -0400

    Added the image reflect

--- app.js
+++ app.js
@@ -6,7 +6,43 @@ const express = require('express')
 
 const app = express()
 
-const controller = async ({query}, res) => {
+const controllerImg = async ({query}, res) => {
+    let {url, ...params} = query
+
+    if (!url)return res.status(406).json({error: 'complete url is required'})
+
+    try{
+        if (url.split("//").length == 1) url = 'https://'+url
+        // Realizar una solicitud GET a la URL de la imagen utilizando axios
+        const {data} = await axios.get(url, {
+          responseType: 'arraybuffer', // Solicitar que Axios devuelva un ArrayBuffer
+        });
+
+        // Establecer los encabezados de la respuesta para indicar que estamos enviando una imagen
+        res.setHeader('Content-Type', 'image/jpeg');
+
+        // Enviar la imagen al cliente utilizando res.end()
+        res.end(data);
+    }catch(e){
+        if (e.message === 'Invalid URL')
+            return res.status(400).json({error: 'invalid url', from: 'vpn'})
+        else if (e.message.includes('Unsupported protocol'))
+            return res.status(400).json({error: `unsupported protocol (${e.message.split('Unsupported protocol ')[1]})`, from: 'vpn'})
+        else if (e.message.includes('connect ECONNREFUSED'))
+            return res.status(400).json({error: 'connection refused', from: 'vpn'})
+        else if (e.message.includes('getaddrinfo ENOTFOUND'))
+            return res.status(404).json({error: `host was not found => ${e.message.split('getaddrinfo ENOTFOUND ')[1]}`, from: 'vpn'})
+        else if (e.message.includes('Request failed with status code'))
+            return res.status(parseInt(e.message.split(' code ')[1])).json({
+                error: `request failed with status code ${e.message.split(' code ')[1]}`,
+                from: 'api',
+                data: e.response.data
+            })
+        return res.status(500).json({msg: 'reenviame el error a mc.dencidance@gmail.com', error: e})
+    }
+}
+
+const controllerJSON = async ({query}, res) => {
 
     let {url, ...params} = query
 
@@ -14,7 +50,8 @@ const controller = async ({query}, res) => {
 
     try{
         if (url.split("//").length == 1) url = 'https://'+url
-        const {data} = await axios.get(url, {params})
+        const {data, ...response} = await axios.get(url, {params})
+        console.log(response);
         return res.json(data)
     }catch(e){
         if (e.message === 'Invalid URL')
@@ -35,7 +72,8 @@ const controller = async ({query}, res) => {
     }
 }
 
-app.get('/', controller)
+app.get('/vpn/img', controllerImg)
+app.get('/vpn/json', controllerJSON)
 
 app.listen(process.env.PORT, () => console.log(`Server running on PORT: ${process.env.PORT}`))
 
